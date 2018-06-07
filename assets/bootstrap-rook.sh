82b828f20233e30d2a5e51266df9f3da46e99f4e
Ben Galewsky <bengal1@illinois.ed>
2018-06-07 16:06:22 -0500

    Make rook volumes survive reboots

--- assets/bootstrap-rook.sh
+++ assets/bootstrap-rook.sh
@@ -2,3 +2,9 @@
 sudo mkfs -t ext4 $1
 sudo mkdir /rook
 sudo mount -t ext4 $1 /rook
+
+# Add the rook mount to fstab to ensure it survives reboot
+lsblk -o NAME,FSTYPE,UUID,MOUNTPOINT $1 \
+     |grep ext4 \
+     | awk '{print "UUID="$3"  "$4"  "$2"  defaults 0 0"}' \
+     >> /etc/fstab
