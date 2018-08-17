45ec2076a9fa2a026af405293457680ca2b1a5f8
Mike Lambert <lambert8@illinois.edu>
2018-08-17 22:23:16 +0000

    Enable/install bash completion on master node

--- assets/bootstrap-master.sh
+++ assets/bootstrap-master.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 cd ~/kubeadm-bootstrap
 sudo -E ./init-master.bash
+
+# Enable kubectl bash completion on on master
+cat > /etc/bash_completion.d/kubectl << __EOF__
+source <(kubectl completion bash)
+__EOF__
