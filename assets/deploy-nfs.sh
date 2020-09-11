57cc37e182808c8d2a203065bb0769eac2e0219f
Mike Lambert <lambert8@illinois.edu>
2020-09-11 20:46:43 +0000

    Update NFS provisioner YAMLs to include all necessary ports

--- assets/deploy-nfs.sh
+++ assets/deploy-nfs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Create the NFS storage class
-kubectl create -f nfs/storageclass.yaml
+kubectl apply -f nfs/storageclass.yaml
 
 # Deploy RBAC role/binding
-kubectl create -f nfs/rbac.yaml
+kubectl apply -f nfs/rbac.yaml
 
 # Create the NFS provisioner
-kubectl create -f nfs/deployment.yaml
+kubectl apply -f nfs/deployment.yaml
