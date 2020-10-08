a3726c945c7d0c5de82b2e262e7702a6a4fe5f20
Mike Lambert <lambert8@illinois.edu>
2020-10-08 09:50:28 -0500

    Small fixes discovered during new cheesehub prod deployment (#17)
    * Update NFS provisioner YAMLs to include all necessary ports
    
    * Add some additional debug output
    
    * Don't bother with kubeadm reset if we're destroying the cluster
    
    * Remove debug branch name

--- assets/deploy-nfs.sh
+++ assets/deploy-nfs.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+echo '============================='
+echo '= Deploying NFS Provisioner ='
+echo '============================='
+
 # Create the NFS storage class
-kubectl create -f nfs/storageclass.yaml
+kubectl apply -f nfs/storageclass.yaml
 
 # Deploy RBAC role/binding
-kubectl create -f nfs/rbac.yaml
+kubectl apply -f nfs/rbac.yaml
 
 # Create the NFS provisioner
-kubectl create -f nfs/deployment.yaml
+kubectl apply -f nfs/deployment.yaml
